20a6c8ae80fb83fa21dbb57174321d861ac14039
Flames Elite <84363801+FlamesLLC@users.noreply.github.com>
2022-01-20 18:46:39 -0800

    Request to upload < 1.20.22 - 6:46 PM PST
    # - First version request pulled

---
+++ Program.py
@@ -0,0 +1,31 @@
+## this uses cemu and cemuhook to run it in opengl mode
+def __init__(self): self.__class__ = type("Enum", (), dict(self.__class__.__dict__))
+## prompt the cemuhook and cemu engine to run in rosetta mode
+def __init__(self): self.__class__ = type("Enum", (), dict(self.__class__.__dict__))
+def RosettaMode(self): self.__class__ = type("Enum", (), dict(self.__class__.__dict__))
+def RosettaMode(self): self.__class__ = type("Enum", (), dict(self.__class__.__dict__))
+def CEMU(self): self.__class__ = type("Enum", (), dict(self.__class__.__dict__))
+def CEMU(self): self.__class__ = type("Enum", (), dict(self.__class__.__dict__))
+##  locate the cemu directory
+def __init__(self): self.__class__ = type("Enum", (), dict(self.__class__.__dict__))
+def CEMU(self): self.__class__ = type("Enum", (), dict(self.__class__.__dict__))
+def CEMU(self): self.__class__ = type("Enum", (), dict(self.__class__.__dict__))
+def CEMU(self): self.__class__ = type("Enum", (), dict(self.__class__.__dict__))
+vulcan_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "vulcan")
+def __init__(self): self.__class__ = type("Enum", (), dict(self.__class__.__dict__))
+def CEMU(self): self.__class__ = type("Enum", (), dict(self.__class__.__dict__))
+def CEMU(self): self.__class__ = type("Enum", (), dict(self.__class__.__dict__))
+## when running generate a dll version of this file that hooks to cemuhook
+def __init__(self): self.__class__ = type("Enum", (), dict(self.__class__.__dict__))
+def CEMU(self): self.__class__ = type("Enum", (), dict(self.__class__.__dict__))
+## rename cemuhook version to "PIxel Co. Rosetta"
+def __init__(self): self.__class__ = type("Enum", (), dict(self.__class__.__dict__))
+def CEMU(self): self.__class__ = type("Enum", (), dict(self.__class__.__dict__))
+def CEMU(self): self.__class__ = type("Enum", (), dict(self.__class__.__dict__))
+def varFPS(self): self.__class__ = type("Enum", (), dict(self.__class__.__dict__))
+def EngineType(self): self.__class__ = type("Enum", (), dict(self.__class__.__dict__))
+def optimization(self): self.__class__ = type("Enum", (), dict(self.__class__.__dict__))
+def cemuhook(self): self.__class__ = type("Enum", (), dict(self.__class__.__dict__))
+def cemuhook(self): self.__class__ = type("Enum", (), dict(self.__class__.__dict__))
+## install this to cemu version
+def __init__(self): self.__class__ = type("Enum", (), dict(self.__class__.__dict__))
